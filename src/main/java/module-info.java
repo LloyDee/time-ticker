dd4d1124f6ef95be7772b47c72aaf6f0f203c302
Lloyd Pintac <ljpintac@gmail.com>
2023-11-03 16:29:46 -0500

    Fixing null values

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -5,6 +5,6 @@ module com.example.helloworldfx {
 
     opens com.javafx to javafx.fxml;
     exports com.javafx;
-    exports com.javafx.codes;
-    opens com.javafx.codes to javafx.fxml;
+//    exports com.javafx.codes;
+//    opens com.javafx.codes to javafx.fxml;
 }
